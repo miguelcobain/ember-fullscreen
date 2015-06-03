604dec429295bf9e860fe69b9f16d1b5f2f69ee4
Miguel Andrade <jmandrade0@gmail.com>
2015-06-03 16:58:42 +0100

    fix ember install

--- blueprints/ember-fullscreen/index.js
+++ blueprints/ember-fullscreen/index.js
@@ -1,6 +1,12 @@
 module.exports = {
   description: 'Add screenfull bower package.',
 
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+
   afterInstall: function() {
     return this.addBowerPackageToProject('screenfull');
   }
